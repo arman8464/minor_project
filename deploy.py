7d975a90bed4cf9cba324cdad06813e052b9c412
arman8464 <126165953+arman8464@users.noreply.github.com>
2024-12-02 21:58:55 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -7,7 +7,10 @@ model = pickle.load(open('savedmodel.sav', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    result = ''
+    return render_template('index.html', **locals())
+
+
 
 
 @app.route('/predict', methods=['POST', 'GET'])
